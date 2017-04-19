ff57b94243674793313a4e8a866759cbab6d7da7
Mark Powell <hi@powellmark.com>
2017-04-19 17:39:16 +1000

    created seed file with 2 speakers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Speaker.destroy_all
+
+speaker1 = Speaker.new
+speaker1.name = "Kasun Maldeni" 
+speaker1.bio = "The best TA in town!" 
+speaker1.linkedin = "www.linkedin.com/" 
+speaker1.image_url = "fillmurray.com/g/400/600" 
+speaker1.talk_title = "Git Good at Git" 
+speaker1.twitter = "@kmaldeni" 
+speaker1.talk_location = "Room 3" 
+speaker1.talk_description = "Rebase that shit" 
+speaker1.job_title = "Master of Commits, Innovator, Saver of Projects" 
+
+speaker1.save
+
+
+speaker2 = Speaker.new
+speaker2.name = "Pauliina Solanne" 
+speaker2.bio = "Queen of the North!" 
+speaker2.linkedin = "www.linkedin.com/" 
+speaker2.image_url = "fillmurray.com/g/400/600" 
+speaker2.talk_title = "Stay Connected" 
+speaker2.twitter = "@p_solanne" 
+speaker2.talk_location = "Room 4" 
+speaker2.talk_description = "Get to the Finnish Line" 
+speaker2.job_title = "Chief Innovator in Chief" 
+
+speaker2.save
