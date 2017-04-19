cc266b5eac316738c0a1361873af806c3cd9631a
Mark Powell <hi@powellmark.com>
2017-04-19 18:19:49 +1000

    added structure for dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get 'user/destroy'
 
-  get 'page/index'
+  # get 'page/index', to: 'page#index'
+
+  get '/dashboard', to: 'dashboard#index'
 
   root 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
