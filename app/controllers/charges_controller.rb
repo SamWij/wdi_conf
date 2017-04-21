9607e1dbbe332762df356c9b8468c0726dd03b4f
Soul Wong <sou.wei.wong@gmail.com>
2017-04-21 14:01:49 +0800

    added user and ticket relationship and automated tickets and user creation

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,8 +1,10 @@
+require 'securerandom'
 class ChargesController < ApplicationController
   def create
   # Amount in cents
-  @amount_cents = (params[:tickets]).to_i * 10000
-  @amount_dollars = (params[:tickets]).to_i * 100
+  @tickets = (params[:tickets]).to_i
+  @amount_cents = @tickets * 10000
+  @amount_dollars = @tickets * 100
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
     :source  => params[:stripeToken]
@@ -15,8 +17,29 @@ class ChargesController < ApplicationController
     :currency    => 'usd'
   )
 
+  if !User.find_by(email: params[:stripeEmail])
+    user = User.new
+    user.email =  params[:stripeEmail]
+    user.first_name = params[:first_name]
+    user.last_name  = params[:last_name]
+    user.password = "123"
+    user.save
+  else
+    user = User.find_by(email: params[:stripeEmail])
+  end
+
+  @tickets.times do |n|
+  ticket = Ticket.new
+  ticket.ticket_number= SecureRandom.uuid
+  ticket.description="WDI CONF Ticket"
+  ticket.cost=100
+  ticket.user_id = user.id
+  ticket.save
+  end
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419084146) do
+ActiveRecord::Schema.define(version: 20170421052436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170419084146) do
     t.decimal  "cost"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 20170419084146) do
     t.string   "password_digest"
   end
 
+  add_foreign_key "tickets", "users"
 end
